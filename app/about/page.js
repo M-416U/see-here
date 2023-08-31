3c8b531cf8a96f6f174cd7b90a5d2db08a2b8b58
M.Mohie <mohamedmohie764@gmail.com>
2023-08-31 22:48:33 +0300

    fix

--- app/about/page.js
+++ app/about/page.js
@@ -1,27 +1,31 @@
+import NavBar from "@/components/NavBar";
 import React from "react";
 
 const About = () => {
   return (
-    <div className="bg-black min-h-screen flex items-center justify-center">
-      <div className="bg-slate-300 p-8 rounded shadow-md max-w-md w-full">
-        <h1 className="text-2xl font-semibold mb-4">About Us</h1>
-        <p className="text-gray-700 mb-4">
-          Welcome to MovieMania! We are a passionate team of movie enthusiasts
-          dedicated to providing you with the latest and greatest in the world
-          of cinema.
-        </p>
-        <p className="text-gray-700 mb-4">
-          Our mission is to share our love for movies and help you discover new
-          films that you'll enjoy. From classic masterpieces to the hottest
-          releases, we've got you covered.
-        </p>
-        <p className="text-gray-700">
-          Our team is made up of movie critics, industry experts, and avid
-          viewers who are excited to curate recommendations and reviews just for
-          you.
-        </p>
+    <>
+      <NavBar home />
+      <div className="bg-black min-h-screen flex items-center justify-center">
+        <div className="bg-slate-300 p-8 rounded shadow-md max-w-md w-full">
+          <h1 className="text-2xl font-semibold mb-4">About Us</h1>
+          <p className="text-gray-700 mb-4">
+            Welcome to MovieMania! We are a passionate team of movie enthusiasts
+            dedicated to providing you with the latest and greatest in the world
+            of cinema.
+          </p>
+          <p className="text-gray-700 mb-4">
+            Our mission is to share our love for movies and help you discover
+            new films that you'll enjoy. From classic masterpieces to the
+            hottest releases, we've got you covered.
+          </p>
+          <p className="text-gray-700">
+            Our team is made up of movie critics, industry experts, and avid
+            viewers who are excited to curate recommendations and reviews just
+            for you.
+          </p>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- app/contact/page.js
+++ app/contact/page.js
@@ -1,6 +1,7 @@
 "use client";
 import React, { useState } from "react";
 import axios from "axios";
+import NavBar from "@/components/NavBar";
 
 const Contact = () => {
   const [formData, setFormData] = useState({
@@ -28,70 +29,73 @@ const Contact = () => {
   };
 
   return (
-    <div className="bg-black min-h-screen flex items-center justify-center">
-      <div className="bg-slte-500 p-8 rounded shadow-md max-w-md w-full">
-        <h1 className="text-2xl font-semibold mb-4">Contact Us</h1>
-        <form onSubmit={handleSubmit}>
-          <div className="mb-4">
-            <label
-              htmlFor="name"
-              className="block text-gray-700 font-medium mb-1"
+    <>
+    <NavBar home/>
+      <div className="bg-black min-h-screen flex items-center justify-center">
+        <div className="bg-slte-500 p-8 rounded shadow-md max-w-md w-full">
+          <h1 className="text-2xl font-semibold mb-4">Contact Us</h1>
+          <form onSubmit={handleSubmit}>
+            <div className="mb-4">
+              <label
+                htmlFor="name"
+                className="block text-gray-700 font-medium mb-1"
+              >
+                Name
+              </label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                value={formData.name}
+                onChange={handleChange}
+                className="w-full border rounded px-3 py-2"
+                required
+              />
+            </div>
+            <div className="mb-4">
+              <label
+                htmlFor="email"
+                className="block text-gray-700 font-medium mb-1"
+              >
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                className="w-full border rounded px-3 py-2"
+                required
+              />
+            </div>
+            <div className="mb-4">
+              <label
+                htmlFor="message"
+                className="block text-gray-700 font-medium mb-1"
+              >
+                Message
+              </label>
+              <textarea
+                id="message"
+                name="message"
+                value={formData.message}
+                onChange={handleChange}
+                className="w-full border rounded px-3 py-2 text-black"
+                rows="4"
+                required
+              />
+            </div>
+            <button
+              type="submit"
+              className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
             >
-              Name
-            </label>
-            <input
-              type="text"
-              id="name"
-              name="name"
-              value={formData.name}
-              onChange={handleChange}
-              className="w-full border rounded px-3 py-2"
-              required
-            />
-          </div>
-          <div className="mb-4">
-            <label
-              htmlFor="email"
-              className="block text-gray-700 font-medium mb-1"
-            >
-              Email
-            </label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-              className="w-full border rounded px-3 py-2"
-              required
-            />
-          </div>
-          <div className="mb-4">
-            <label
-              htmlFor="message"
-              className="block text-gray-700 font-medium mb-1"
-            >
-              Message
-            </label>
-            <textarea
-              id="message"
-              name="message"
-              value={formData.message}
-              onChange={handleChange}
-              className="w-full border rounded px-3 py-2 text-black"
-              rows="4"
-              required
-            />
-          </div>
-          <button
-            type="submit"
-            className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
-          >
-            Send Message
-          </button>
-        </form>
+              Send Message
+            </button>
+          </form>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
