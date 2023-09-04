eac4ab5503c6ffd2f2360173e8e6dd22434f09c1
M.Mohie <mohamedmohie764@gmail.com>
2023-09-04 21:00:09 +0300

    fix google anylistic

--- app/layout.js
+++ app/layout.js
@@ -1,23 +1,31 @@
-"use client";
+import Script from "next/script";
 import "./globals.css";
 import { Ubuntu_Mono } from "next/font/google";
-import { useEffect } from "react";
-import { initGA, logPageView } from "./google-analytics";
 const ubuntu = Ubuntu_Mono({ subsets: ["latin"], weight: "400" });
 
 export default function RootLayout({ children }) {
-  useEffect(() => {
-    initGA();
-    logPageView();
-  }, []);
   return (
     <html lang="en">
-      <script
-        async
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9570051132890232"
-        crossorigin="anonymous"
-      ></script>
-      <body className={ubuntu.className}>{children}</body>
+      <body className={ubuntu.className}>
+        <div className="container">
+          <Script
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9570051132890232"
+            crossorigin="anonymous"
+          />
+          <Script src="https://www.googletagmanager.com/gtag/js?id=G-DLMRSMKD1N" />
+          <Script id="google-analytics">
+            {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', 'G-DLMRSMKD1N');
+        `}
+          </Script>
+        </div>
+        {children}
+      </body>
     </html>
   );
 }
