abd9686bb59549e3162d2483c22a5c675b321440
M.Mohie <mohamedmohie764@gmail.com>
2023-09-06 05:08:37 +0300

    solve image

--- app/Images/Images.js
+++ app/Images/Images.js
@@ -1,13 +1,12 @@
-import { useTheme } from "next-themes";
-import logo from "./Logo.png";
-import lightLogo from "./lightLogo.png";
 import Image from "next/image";
+import logo from "./Logo.png";
 
 const Logo = () => {
-  const { theme } = useTheme();
   const logoSrc = logo;
 
-  return <Image src={logoSrc} alt="Logo" className="w-[83px] h-[69px] relative" />;
+  return (
+    <Image src={logoSrc} alt="Logo" className="w-[83px] h-[69px] relative" />
+  );
 };
 
 export default Logo;
